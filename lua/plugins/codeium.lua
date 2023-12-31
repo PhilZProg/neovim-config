a1503bb01f7c5c2259e29565a40d8428acd4d2dc
PhilZProg <philzprog@ya.ru>
2023-12-31 13:06:56 +0300

    feat: changed keybindings that matched with autocompletions

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,16 +1,16 @@
 return {
 	"Exafunction/codeium.vim",
 	config = function()
-		vim.keymap.set("i", "<C-g>", function()
+		vim.keymap.set("i", "<A-g>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-n>", function()
+		vim.keymap.set("i", "<A-n>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-b>", function()
+		vim.keymap.set("i", "<A-b>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-x>", function()
+		vim.keymap.set("i", "<A-x>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true, silent = true })
 	end,
