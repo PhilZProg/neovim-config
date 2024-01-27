1b9a55df127356ff9cef94dd93f323c8398dea1a
Захаров Филипп <philzprog@ya.ru>
2024-01-27 22:54:08 +0300

    refactor: turn off virtual text
    For some reason when I deubgg with the virtual text plugin it breaks the debugging process.
    Needs more looking into.

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,87 +1,128 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function()
+			-- DAP UI setup
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			-- DAP adapter setup
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "--quiet", "--interpreter=dap" },
+			}
+
+			-- DAP configurations
+			dap.configurations.c = {
+				{
+					name = "Run executable (GDB)",
+					type = "gdb",
+					request = "launch",
+					-- This requires special handling of 'run_last', see
+					-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+					program = function()
+						local path = vim.fn.input({
+							prompt = "Path to executable: ",
+							default = vim.fn.getcwd() .. "/",
+							completion = "file",
+						})
+
+						return (path and path ~= "") and path or dap.ABORT
+					end,
+				},
+				{
+					name = "Run executable with arguments (GDB)",
+					type = "gdb",
+					request = "launch",
+					-- This requires special handling of 'run_last', see
+					-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+					program = function()
+						local path = vim.fn.input({
+							prompt = "Path to executable: ",
+							default = vim.fn.getcwd() .. "/",
+							completion = "file",
+						})
+
+						return (path and path ~= "") and path or dap.ABORT
+					end,
+					args = function()
+						local args_str = vim.fn.input({
+							prompt = "Arguments: ",
+						})
+						return vim.split(args_str, " +")
+					end,
+				},
+				{
+					name = "Attach to process (GDB)",
+					type = "gdb",
+					request = "attach",
+					processId = require("dap.utils").pick_process,
+				},
+			}
+
+			dap.configurations.cpp = dap.configurations.c
+
+			-- DAP keymaps
+			vim.keymap.set("n", "<F10>", dap.step_over, {})
+			vim.keymap.set("n", "<F11>", dap.step_into, {})
+			vim.keymap.set("n", "<F12>", dap.step_out, {})
+			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		end,
 	},
-	config = function()
-		-- DAP UI setup
-		require("dapui").setup()
-		local dap, dapui = require("dap"), require("dapui")
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+-- {
+-- 		"theHamsta/nvim-dap-virtual-text",
+-- 		config = function()
+-- 			require("nvim-dap-virtual-text").setup({
+-- 				enabled = true, -- enable this plugin (the default)
+-- 				enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+-- 				highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+-- 				highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+-- 				show_stop_reason = true, -- show stop reason when stopped for exceptions
+-- 				commented = false, -- prefix virtual text with comment string
+-- 				only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+-- 				all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+-- 				clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+-- 				virt_text_pos = "inline", -- position of virtual text, see `:h nvim_buf_set_extmark()`
+--         --- A callback that determines how a variable is displayed or whether it should be omitted
+-- 				--- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+-- 				--- @param buf number
+-- 				--- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+-- 				--- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+-- 				--- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+-- 				--- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+-- 				display_callback = function(variable, buf, stackframe, node, options)
+-- 					if options.virt_text_pos == "inline" then
+-- 						return " = " .. variable.value
+-- 					else
+-- 						return variable.name .. " = " .. variable.value
+-- 					end
+-- 				end,
+-- 				-- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+-- 				virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol",
 
-		-- DAP adapter setup
-		dap.adapters.gdb = {
-			type = "executable",
-			command = "gdb",
-			args = { "--quiet", "--interpreter=dap" },
-		}
-
-		-- DAP configurations
-		dap.configurations.c = {
-			{
-				name = "Run executable (GDB)",
-				type = "gdb",
-				request = "launch",
-				-- This requires special handling of 'run_last', see
-				-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
-				program = function()
-					local path = vim.fn.input({
-						prompt = "Path to executable: ",
-						default = vim.fn.getcwd() .. "/",
-						completion = "file",
-					})
-
-					return (path and path ~= "") and path or dap.ABORT
-				end,
-			},
-			{
-				name = "Run executable with arguments (GDB)",
-				type = "gdb",
-				request = "launch",
-				-- This requires special handling of 'run_last', see
-				-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
-				program = function()
-					local path = vim.fn.input({
-						prompt = "Path to executable: ",
-						default = vim.fn.getcwd() .. "/",
-						completion = "file",
-					})
-
-					return (path and path ~= "") and path or dap.ABORT
-				end,
-				args = function()
-					local args_str = vim.fn.input({
-						prompt = "Arguments: ",
-					})
-					return vim.split(args_str, " +")
-				end,
-			},
-			{
-				name = "Attach to process (GDB)",
-				type = "gdb",
-				request = "attach",
-				processId = require("dap.utils").pick_process,
-			},
-		}
-
-		dap.configurations.cpp = dap.configurations.c
-
-		-- DAP keymaps
-		vim.keymap.set("n", "<F10>", dap.step_over, {})
-		vim.keymap.set("n", "<F11>", dap.step_into, {})
-		vim.keymap.set("n", "<F12>", dap.step_out, {})
-		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-	end,
+-- 				-- experimental features:
+-- 				all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+-- 				virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+-- 				virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+-- 				-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+-- 			})
+-- 		end,
+-- 	},
 }
