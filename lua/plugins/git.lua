b1b36a95708973d5d3b98596864bf520360c5405
Захаров Филипп <f.zaharov@quanttelecom.ru>
2024-01-31 13:44:35 +0300

    feat: изменен бинд, добавлен desc

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,7 +4,8 @@ return {
 		config = function()
 			require("gitsigns").setup()
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
+			vim.keymap.set("n", "<leader>gpi", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview Hunk Inline" })
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
 		end,
 	},
 	{
