7150a88a7034958a17a7ecd9590b3fdea1e75684
Захаров Филипп <f.zaharov@quanttelecom.ru>
2024-01-31 10:17:17 +0300

    refactor: удаление старого функционала
    Мой конфиг nvim уже давно определяет новые хайлайты автоматически,
    указывать их в ручную нет смысла.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-			-- ensure_installed = { "lua", "c", "cpp", "devicetree" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
