87bf77f1cc67dba0d5beeb57fe5ea92ef8f684dc
Захаров Филипп <f.zaharov@quanttelecom.ru>
2024-02-12 12:15:48 +0300

    feat: additional functionality for clangd
    Clangd should display inlay hints but it doesnt for some reason. Needs fixing.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,11 +12,10 @@ return {
 		opts = {
 			auto_install = true,
 		},
-		-- config = function()
-		-- 	require("mason-lspconfig").setup({
-		-- 		ensure_installed = { "lua_ls", "clangd"},
-		-- 	})
-		-- end,
+		config = function()
+			require("clangd_extensions.inlay_hints").setup_autocmd()
+			require("clangd_extensions.inlay_hints").set_inlay_hints()
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
